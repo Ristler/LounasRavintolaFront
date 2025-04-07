b991d4ae31dd5372a5a1bb65f9b876f40b181807
Daniel <100704382+Ristler@users.noreply.github.com>
2025-04-07 20:33:50 +0300

    Github pages
    Pages

--- src/components/landing.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-import { Card } from "antd";
-export default function Landing() {
-  return (
-    <div>landing</div>
-  )
-}
--- src/components/orderFood.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-
-
-export default function OrderFood() {
-  return (
-    <p>Moikka olen ruokasivu</p>
-  );
-}
